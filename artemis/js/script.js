9bdc15759925ea357e77b9bc4888b511b3628471
pepijnemmers <pepijn.emmers@hotmail.com>
2022-03-07 20:30:11 +0100

    slider added

--- artemis/js/script.js
+++ artemis/js/script.js
@@ -1,3 +1,13 @@
-$(document).ready(function(){
+$(document).ready(function() {
     
+    // Large slick slider with center mode
+    $('.slick-center-lg').slick({
+        dots: true,
+        arrows: true,
+        centerMode: true,
+        centerPadding: '0px',
+        slidesToShow: 3
+    });
+
+
 });
