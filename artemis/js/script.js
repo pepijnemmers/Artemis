9400788aab4dab92805d5f11190066c9990e0722
pepijnemmers <pepijn.emmers@hotmail.com>
2022-03-08 17:53:57 +0100

    homepage added contact section and footer

--- artemis/js/script.js
+++ artemis/js/script.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {
+
+    // Change header once scrolled
+    $(window).scroll(function(){
+        if ($(window).scrollTop() > 100) {
+            $("header").addClass("scrolled")
+        } else {
+            $("header").removeClass("scrolled")
+        }
+    })
     
     // Large slick slider with center mode
     $('.slick-center-lg').slick({
@@ -9,5 +18,14 @@ $(document).ready(function() {
         slidesToShow: 3
     });
 
+    
 
 });
+
+// contact form
+function submitForm() {
+    document.getElementById("contact-form").reset();
+}
+
+// footer year 
+document.getElementById("currentYear").innerHTML = new Date().getFullYear();
