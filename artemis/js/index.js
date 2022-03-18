b50f8decab86d4c6d5c46ef296937aa90b1a2506
pepijnemmers <pepijn.emmers@hotmail.com>
2022-03-18 17:03:23 +0100

    gsap fixes

--- artemis/js/index.js
+++ artemis/js/index.js
@@ -1,5 +1,5 @@
 const tl = gsap.timeline({ defaults: { ease: "Power3.easeOut", duration: 2 } });
-tl.delay(1);
+tl.delay(.5);
 
 tl.to(".hero-text h1", { 'clip-path': 'polygon(0 0, 105% 0, 105% 105%, 0% 105%)', opacity: 1, y: 0 });
 tl.to(".hero-text p", { opacity: 1, y: 0 }, "-=1");
